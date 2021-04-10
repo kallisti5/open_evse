4d1b99d6bddd479b3aaaa72e644e30b8f51be1ac
Alexander von Gluck IV <kallisti5@unixzen.com>
2021-04-10 17:30:11 -0500

    pio: Add US firmware build support

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,11 +3,13 @@
 # Script to compile all released enviroments and copy build artifacts to project root for easy upload to github releases. 
 
 pio run -e emonevse
-pio run -e openevse
+pio run -e openevse-us
+pio run -e openevse-eu
 pio run -e emonevse-3ph
 
 cp .pio/build/emonevse/firmware.hex emonevse.hex
-cp .pio/build/openevse/firmware.hex openevse.hex
+cp .pio/build/openevse-us/firmware.hex openevse-us.hex
+cp .pio/build/openevse-eu/firmware.hex openevse-eu.hex
 cp .pio/build/emonevse-3ph/firmware.hex emonevse-3ph.hex
 ls *.hex
 
